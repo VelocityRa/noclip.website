7a18777e78c2e43ed3f920f8f4e01e743e0b5c69
Nick Renieris <velocityra@gmail.com>
2020-12-29 05:40:30 +0200

    ambient lighting experiments, flag layers, etc

--- src/SlyCooper/SlyData.ts
+++ src/SlyCooper/SlyData.ts
@@ -1,12 +1,11 @@
 import ArrayBufferSlice from '../ArrayBufferSlice';
-import { vec2, vec3 } from 'gl-matrix';
 import { SceneGroup, SceneDesc, SceneGfx, ViewerRenderInput } from "../viewer";
 import * as Viewer from "../viewer";
-import { assert, hexzero, hexzero0x, leftPad } from '../util';
+import { assert, hexzero, hexzero0x, spacePad } from '../util';
 import { downloadCanvasAsPng, downloadText } from "../DownloadUtils";
 import { range, range_end } from '../MathHelpers';
 import * as Settings from './SlyConstants';
-import { vec4, vec3, mat4, ReadonlyMat4, ReadonlyVec3 } from "gl-matrix";
+import { vec2, vec3, vec4, mat4, ReadonlyMat4, ReadonlyVec3 } from "gl-matrix";
 import { drawWorldSpaceAABB, getDebugOverlayCanvas2D, drawWorldSpacePoint } from "../DebugJunk";
 
 // TODO: move?
@@ -29,7 +28,7 @@ export class DataStream {
 
 interface CLUTMetaEntry {
     colorCount: number;
-    size: number;
+    colorSize: number;
     offset: number; // Relative to start of color palette data
 }
 
@@ -38,11 +37,11 @@ export function parseCLUTMetaEntries(stream: DataStream): CLUTMetaEntry[] {
     let entries = new Array<CLUTMetaEntry>();
 
     for (let i = 0; i < entryCount; ++i) {
-        stream.offs += 4;
+        const unk0 = stream.readUint32(); // TODO
         const colorCount = stream.readUint16();
-        const size = stream.readUint16();
+        const colorSize = stream.readUint16();
         const offset = stream.readUint32();
-        entries.push({ colorCount: colorCount, size: size, offset: offset });
+        entries.push({ colorCount, colorSize, offset });
     }
 
     return entries;
@@ -122,37 +121,83 @@ function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function expand5to8(n: number) {
+    return (n << (8 - 5)) | (n >>> (10 - 8));
+}
+
+function expand6to8(n: number) {
+    return (n << (8 - 6)) | (n >>> (12 - 8));
+}
+
+function bswap16(low: number, high: number) {
+    return (high << 8) | low;
+}
+
 export class Texture {
     public texels_rgba: Uint8Array;
     public viewerTexture: Viewer.Texture[] = [];
 
     private static csm1ClutIndices = getCsm1ClutIndices();
     constructor(paletteBuf: ArrayBufferSlice, imageBuf: ArrayBufferSlice,
-        public width: number, public height: number, colorCount: number, public name: string = "N/A") {
+        public width: number, public height: number, colorCount: number, colorSize: number, public name: string = "N/A") {
 
-        // TODO
         this.texels_rgba = new Uint8Array(width * height * 4);
 
         let texels_slice = imageBuf.createTypedArray(Uint8Array);
         let palette_slice = paletteBuf.createTypedArray(Uint8Array);
 
-        for (let i of range_end(0, width * height)) {
-            let idx = texels_slice[i] * 4;
-            if (colorCount == 256)
-                idx = Texture.csm1ClutIndices[texels_slice[i]] * 4;
+        // const x = i % width;
+        // const y = Math.floor(i / width);
+        // const inv_y = (-(y - (width / 2))) + width / 2 - 1;
+        // const offs = (inv_y * width + x) * 4;
 
-            // const x = i % width;
-            // const y = Math.floor(i / width);
-            // const inv_y = (-(y - (width / 2))) + width / 2 - 1;
-            // const offs = (inv_y * width + x) * 4;
-            const offs = i * 4;
+        if (colorSize == 4) {
+            for (let i of range_end(0, width * height)) {
+                let idx: number;
+                if (colorCount == 256)
+                    idx = Texture.csm1ClutIndices[texels_slice[i]] * colorSize;
+                else
+                    idx = texels_slice[i] * colorSize;
 
-            // const alpha = palette_slice[idx + 3] / 256; // Paint alpha black
-            this.texels_rgba[offs + 0] = palette_slice[idx + 0];
-            this.texels_rgba[offs + 1] = palette_slice[idx + 1];
-            this.texels_rgba[offs + 2] = palette_slice[idx + 2];
-            this.texels_rgba[offs + 3] = palette_slice[idx + 3]; // 0xFF; // palette_slice[idx + 3] * 2;
+                const offs = i * 4;
+
+                this.texels_rgba[offs + 0] = palette_slice[idx + 0];
+                this.texels_rgba[offs + 1] = palette_slice[idx + 1];
+                this.texels_rgba[offs + 2] = palette_slice[idx + 2];
+                this.texels_rgba[offs + 3] = palette_slice[idx + 3];
+            }
+        } else {
+            for (let i of range_end(0, width * height)) {
+                const offs = i * 4;
+
+                const col = (i % 10 == 0) ? 0xFF : 0x00;
+                this.texels_rgba[offs + 0] = col;
+                this.texels_rgba[offs + 1] = 0x00;
+                this.texels_rgba[offs + 2] = col;
+                this.texels_rgba[offs + 3] = 0xFF;
+            }
         }
+        // } else if (colorSize == 2) {
+        //     for (let i of range_end(0, width * height)) {
+        //         let idx: number;
+        //         if (colorCount == 256)
+        //             idx = Texture.csm1ClutIndices[texels_slice[i]] * colorSize;
+        //         else
+        //             idx = texels_slice[i] * colorSize;
+
+        //         const offs = i * 4;
+
+        //         const low = palette_slice[idx];
+        //         const high = palette_slice[idx + 1];
+        //         const p = bswap16(high, low)
+        //         this.texels_rgba[offs + 0] = expand5to8(p & 0x1F);
+        //         this.texels_rgba[offs + 1] = expand6to8((p >>> 5) & 0x3F);
+        //         this.texels_rgba[offs + 2] = expand5to8((p >>> 11) & 0x1F);
+        //         this.texels_rgba[offs + 3] = 0xFF; // ((p >>> 15) & 1) * 255;
+        //     }
+        // } else {
+        //     console.warn(`Unsupported colorSize ${colorSize}`);
+        // }
     }
 
     public async toCanvas(): Promise<Viewer.Texture> {
@@ -177,7 +222,7 @@ export class Texture {
         }
 
         const extraInfo = new Map<string, string>();
-        extraInfo.set('Format', 'IDTEX8-CSM1 (PS2)');
+        extraInfo.set('Format', 'IDTEX8-CSM1 (PS2)'); // todo: wrong
 
 
         return { name: this.name, surfaces, extraInfo };
@@ -214,16 +259,34 @@ export interface MeshChunk {
     unkIndices: Uint16Array;
 
     szme: SzmeChunk | undefined;
+
+    name: string; // for debugging
+    flags: number; // redundant since it's the same as the parent Mesh, used for debugging (layers system)
 }
 
 export let totalChunkIndex = 0;
+
+/*
+0b10110 (0x16) contains lights (so, emissive material?)
+*/
+enum MeshFlag {
+    None = 0,
+
+    Static = 1 << 1,
+    NoShading = 1 << 2, // maybe
+    Skybox = 1 << 3,
+    TreasureKeys = 1 << 5,
+    Coins = 1 << 7, // and glow part of treasture keys (?)
+}
 
 export class Mesh {
     static readonly szmsMagic = 0x534D5A53; // "SZMS"
     static readonly szmeMagic = 0x454d5a53; // "SZME"
 
+    public flags: number;
     public chunks: MeshChunk[] = [];
     public szmeHeader: (SzmeHeader | undefined);
+
 
     constructor(buffer: ArrayBufferSlice, public offset: number, public index: number) {
         const view = buffer.createDataView(0);
@@ -232,7 +295,7 @@ export class Mesh {
 
         // Read SZMS header
 
-        const flags = stream.readUint16();
+        this.flags = stream.readUint16();
 
         if (stream.readUint32() != Mesh.szmsMagic)
             throw new Error(`bad SZMS magic`);
@@ -240,8 +303,8 @@ export class Mesh {
         if (stream.readUint32() != 0x4)
             throw new Error(`unsupported version`);
 
-        if ((flags & 1) != 0)
-            throw new Error(`skipping mesh at ${hexzero0x(offset)}, has invalid flags: ${hexzero0x(flags)}`);
+        if ((this.flags & 1) != 0)
+            throw new Error(`skipping mesh at ${hexzero0x(offset)}, has invalid flags: ${hexzero0x(this.flags)}`);
 
         const totalSize = stream.readUint32();
 
@@ -336,8 +399,15 @@ export class Mesh {
                 unkIndices[i] = stream.readUint16();
             }
 
+            let szmsAddr = offset - 2;
+            let szmeAddr = startOffs + meshOffs;
+            let submeshCount = spacePad(meshOffsets.length.toString(), 2);
+            let name = `SZMS ${hexzero(szmsAddr, 6)} ${hexzero(this.flags, 3)} ${submeshCount} | ${hexzero(szmeAddr)}`;
+
+            let flags = this.flags; // redundant
+
             let szme: (SzmeChunk | undefined) = undefined;
-            this.chunks.push({ positions, normals, texCoords, vertexColor, vertexColorFloats, trianglesIndices, unkIndices, szme});
+            this.chunks.push({ positions, normals, texCoords, vertexColor, vertexColorFloats, trianglesIndices, unkIndices, szme, name, flags });
         }
 
         // Read SZME header
@@ -347,36 +417,36 @@ export class Mesh {
         if (stream.readUint32() != Mesh.szmeMagic)
             throw new Error(`bad SZME magic at ${hexzero0x(szmeOffs)}`);
 
-        if ((flags & 2) != 0) {
+        if ((this.flags & MeshFlag.Static) != 0) {
             stream.readUint32();
         }
-        if ((flags & 0x200) != 0) {
+        if ((this.flags & 0x200) != 0) {
             stream.readFloat32();
         }
-        if ((flags & 4) != 0) {
+        if ((this.flags & 4) != 0) {
             stream.readFloat32();
         }
-        if ((flags & 8) != 0) {
+        if ((this.flags & MeshFlag.Skybox) != 0) {
             stream.readFloat32();
         }
-        if ((flags & 0x10) != 0) {
+        if ((this.flags & 0x10) != 0) {
             stream.readFloat32();
         }
-        if ((flags & 0x20) != 0) {
+        if ((this.flags & 0x20) != 0) {
             stream.readFloat32();
         }
-        if ((flags & 0x40) != 0) {
+        if ((this.flags & 0x40) != 0) {
             // meta entry alt
             //Assert(false);
-            console.warn(`skipping chunks at ${hexzero0x(stream.offs)}, 0x40 flags unsupported: ${hexzero0x(flags, 3)}`);
+            console.warn(`skipping chunks at ${hexzero0x(stream.offs)}, 0x40 this.flags unsupported: ${hexzero0x(this.flags, 3)}`);
             stream.offs += 0x1C;
         }
-        if ((flags & 0x80) != 0) {
+        if ((this.flags & 0x80) != 0) {
             stream.readFloat32(); stream.readFloat32(); stream.readFloat32(); // vec3
             stream.readFloat32(); stream.readFloat32(); stream.readFloat32(); stream.readFloat32(); // vec4
         }
 
-        if (((flags & 0x100) == 0)) {
+        if (((this.flags & 0x100) == 0)) {
             const position: vec3 = stream.readVec3();
 
             const unkFloat0x14 = stream.readFloat32();
@@ -460,7 +530,7 @@ export class Mesh {
 
             this.szmeHeader = { position };
         } else {
-            console.warn(`skipping szme data at ${szmeOffs}, has invalid flags: ${hexzero0x(flags)}`);
+            console.warn(`skipping szme data at ${szmeOffs}, has invalid flags: ${hexzero0x(this.flags)}`);
         }
     }
 }
