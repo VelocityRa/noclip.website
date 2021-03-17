a5cc3798ff10bbc17097b58f7c3380f8b16a75f3
Nick Renieris <velocityra@gmail.com>
2021-03-17 11:35:47 +0200

    sly2: second texture id

--- src/SlyCooper/SlyConstants.ts
+++ src/SlyCooper/SlyConstants.ts
@@ -1,7 +1,7 @@
 export const PARSE_MESHES = true;
 export const MESH_EXPORT = true;
 export const SEPARATE_OBJECT_SUBMESHES = true;
-export const MESH_SCALE = 1 / 1000.0;
+export const MESH_SCALE = 1 / 100.0;
 export const SHOW_ALL_MESHES = false;
 
 export const TEXTURES_EXPORT = true;
